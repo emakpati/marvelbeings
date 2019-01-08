093e5078eddc3f8a8c126f913048bc6982b49bc3
Ekene Akpati <emakpati@gmail.com>
2019-01-08 09:20:36 -0500

    Update scraper and tweet

--- scraper.py
+++ scraper.py
@@ -14,22 +14,19 @@ char_content = soup.find_all("div", class_="mvl-card mvl-card--explore")
 
 
 # Get url to individual character page.
-def get_url(content):
-    i = 7  # First five are not characters.
+def get_url(content, i):
+    # First five are not characters.
     html_block = str(content[i])  # From list of divs convert into str.
     temp_path = html_block.split('url="', 1)  # Single out the url.
     path = temp_path[1].split('" data-further-details', 1)[0]
-    i += 1  # Increment for next div.
     char_url = "https://www.marvel.com"+path
     return char_url
 
 # Get name of individual character.
-def get_name(content):
-    i = 7
+def get_name(content, i):
     html_block = str(content[i])
     temp_name = html_block.split('data-click-text="', 1)
     character = temp_name[1].split('" data-click-type')[0]
-    i += 1
     return character
 
 
--- tweet.py
+++ tweet.py
@@ -2,7 +2,10 @@ import setup, scraper as sc, tweepy, time
 
 api = tweepy.API(setup.auth)
 
+i = 6
+
 for content in sc.char_content:
-    api.update_status("Today's Marvel being is " + sc.get_name(sc.char_content).upper() + "! #Marvel #MarvelComics #Heroes #Villains " + sc.get_url(sc.char_content))
+    api.update_status("Today's Marvel being is " + sc.get_name(sc.char_content,i).upper() + "! #Marvel #MarvelComics #Heroes #Villains " + sc.get_url(sc.char_content,i))
+    i += 1
     time.sleep(10)  # Sleep for 86400 sec (24 hours).
 
